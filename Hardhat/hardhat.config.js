d9aa34288d20cfda635b79966982f9a5d72edd1c
Raad05 <ashabulraad@gmail.com>
2024-02-03 23:45:55 +0600

    hardhat network configured

--- Hardhat/hardhat.config.js
+++ Hardhat/hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const { ALCHEMY_RPC_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: ALCHEMY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };

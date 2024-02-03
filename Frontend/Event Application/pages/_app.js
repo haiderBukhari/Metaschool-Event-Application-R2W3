16a9c4342020ea86a684a20c4ccc571bbb76c8e7
Raad05 <ashabulraad@gmail.com>
2024-02-03 22:13:10 +0600

    wallet interaction

--- Frontend/Event Application/pages/_app.js
+++ Frontend/Event Application/pages/_app.js
@@ -1,5 +1,30 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+// import rainbowkit
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { celo, celoAlfajores, celoCannoli } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+// const { chains, publicClient } = configureChains(
+//   [mainnet, polygon, sepolia],
+//   [alchemyProvider({ apiKey: process.env.ALCHEMY_RPC_URL }), publicProvider()]
+// );
+
+// const { connectors } = getDefaultWallets({
+//   appName: "EVENT APPLICATION",
+//   projectId: "3c0117be390fa0e7fefa97a8363f7b5f",
+//   chains,
+// });
+
+// const wagmiConfig = createConfig({
+//   autoConnect: true,
+//   connectors,
+//   publicClient,
+// });
 
 export default function App({ Component, pageProps }) {
   return (
